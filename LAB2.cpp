0d6858e37936b2aa00ea043ea11be03ab2011d0c
Danexy123 <111960893+Danexy123@users.noreply.github.com>
2022-11-05 18:10:45 +0500

    Add files via upload

---
+++ LAB2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream> //подключаем основную библиотеку в с++
+
+using namespace std; //подключаем основное пространство имен
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    int sum = 0;
+
+    for (int a = 0; a != 200; a++)
+    {
+        if (a % 3 == 0)
+        {
+            sum += a;
+        }
+    }
+    cout << "Сумма чисел = " << sum << endl;
+    return 0;
+}
+
